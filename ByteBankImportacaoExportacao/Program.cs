169cc7e8fb307d58de9b5adb4629eb53906a1074
Ricardo <ricardocavalcantesilva@gmail.com>
2019-01-24 00:13:13 -0200

    lendo o arquivo de contas e escrevendo o arquivo na tela na codificação utf8

--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,34 @@ namespace ByteBankImportacaoExportacao
     {
         static void Main(string[] args)
         {
+            var arquivo = "contas.txt";
+
+            var fluxoDoArquivo = new FileStream(arquivo, FileMode.Open);
+
+            var buffer = new byte[1024];
+
+            var quantidadeDeBytesLidos = -1;
+            while (quantidadeDeBytesLidos != 0)
+            {
+                quantidadeDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void EscreverBuffer(byte[] buffer)
+        {
+            var encoding = new UTF8Encoding();
+            var texto = encoding.GetString(buffer);
+
+            Console.Write(texto);
+
+            //foreach (var meuByte in buffer)
+            //{
+            //    Console.Write(meuByte);
+            //    Console.Write(" ");
+            //}
         }
     }
 }
