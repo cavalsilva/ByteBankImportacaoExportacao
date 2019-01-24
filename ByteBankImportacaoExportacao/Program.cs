3e57bd9ca39c4871796efb6e0f70aff8e350aad9
Ricardo <ricardocavalcantesilva@gmail.com>
2019-01-24 00:52:40 -0200

    Inclusao do método flush

--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -59,5 +59,24 @@ namespace ByteBankImportacaoExportacao
             }
         }
 
+        static void TestaEscrita()
+        {
+            var caminhoArquivo = "teste.txt";
+
+            using (var fluxoDeArquivo = new FileStream(caminhoArquivo, FileMode.Create))
+            using (var escritor = new StreamWriter(fluxoDeArquivo))
+            {
+                for (int i = 0; i < 100000000; i++)
+                {
+                    escritor.WriteLine($"Linha {i}");
+                    // O comando abaixo escreve o conteúdo no arquivo
+                    escritor.Flush();
+
+                    Console.WriteLine($"Linha {i} foi escrita no arquivo. Tecle enter p adicionar mais uma!");
+                    Console.ReadLine();
+                }
+            }
+        }
+
     }
 }
