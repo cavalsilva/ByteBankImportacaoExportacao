2fa99ed693b28b2989ebc2c7646fc0657bd1a11e
Ricardo <ricardocavalcantesilva@gmail.com>
2019-01-24 01:02:34 -0200

    Gravando arquivo utilizando Console com streams

--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -12,8 +12,10 @@ namespace ByteBankImportacaoExportacao
     {
         static void Main(string[] args)
         {
-            var arquivo = "contas.txt";
 
+            //Bloco de leitura de arquivos
+            var arquivo = "contas.txt";
+            
             //Quando existir um using abaixo o outro, não é necessário ter {} nos acimas
             using (var fluxoDoArquivo = new FileStream(arquivo, FileMode.Open))
             using (var leitor = new StreamReader(fluxoDoArquivo))
@@ -27,7 +29,24 @@ namespace ByteBankImportacaoExportacao
                 }
             }
 
-            Console.ReadLine();
+
+            //Bloco de escrita de arquivos via console
+            using (var fluxoDeEntrada = Console.OpenStandardInput())
+            using (var fs = new FileStream("entradaConsole.txt", FileMode.Create))
+            {
+                var buffer = new byte[1024];
+
+                while (true)
+                {
+                    var bytesLidos = fluxoDeEntrada.Read(buffer, 0, 1024);
+
+                    fs.Write(buffer, 0, bytesLidos);
+                    fs.Flush();
+
+                    Console.WriteLine($"Bytes lidos da console: {bytesLidos}");
+                }
+            }
+
         }
 
         static ContaCorrente ConverterStringParaContaCorrente(string linha)
