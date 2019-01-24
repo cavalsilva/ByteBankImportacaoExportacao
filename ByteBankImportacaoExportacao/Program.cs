863aec59cd477aff25a8246c2bca72d7a7ee55c2
Ricardo <ricardocavalcantesilva@gmail.com>
2019-01-24 00:48:21 -0200

    Alteração no arquivo contas.txt para ficar no padrão CSV, utilização dos métodos int.Parse e double.Parse, escrever diretamento no stream, usar StreamWriter

--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -20,19 +21,43 @@ namespace ByteBankImportacaoExportacao
                 while (!leitor.EndOfStream)
                 {
                     var linha = leitor.ReadLine();
-                    Console.WriteLine(linha);
+                    var contaCorrente = ConverterStringParaContaCorrente(linha);
+                    var msg = $"{contaCorrente.Titular.Nome}: Conta número {contaCorrente.Numero}, ag. {contaCorrente.Agencia}. Saldo: {contaCorrente.Saldo}";
+                    Console.WriteLine(msg);
                 }
             }
 
             Console.ReadLine();
         }
 
-        //static void EscreverBuffer(byte[] buffer, int bytesLidos)
-        //{
-        //    var encoding = new UTF8Encoding();
-        //    var texto = encoding.GetString(buffer, 0, bytesLidos);
+        static ContaCorrente ConverterStringParaContaCorrente(string linha)
+        {
+            string[] campos = linha.Split(',');
 
-        //    Console.Write(texto);
-        //}
+            var agencia = int.Parse(campos[0]);
+            var numero = int.Parse(campos[1]);
+            var saldo = double.Parse(campos[2].Replace('.',','));
+            var nomeTitular = campos[3];
+
+            var titular = new Cliente();
+            titular.Nome = nomeTitular;
+
+            var resultado = new ContaCorrente(agencia, numero);
+            resultado.Depositar(saldo);
+            resultado.Titular = titular;
+
+            return resultado;
+        }
+
+        static void CriarArquivo()
+        {
+            var caminhoNovoArquivo = "contasExportadas.csv";
+            using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+            using (var escritor = new StreamWriter(fluxoDeArquivo, Encoding.UTF8))
+            {
+                escritor.Write("456,78945,4785.50,Ricardo Silva");
+            }
+        }
+
     }
 }
